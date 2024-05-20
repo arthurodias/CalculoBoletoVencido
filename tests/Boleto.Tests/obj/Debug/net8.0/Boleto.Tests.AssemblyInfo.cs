1593dccfbea775a584b1abe67d77f23bbb5e779b
Arthur Rodrigues Dias <arthur.rodias@gmail.com>
2024-05-20 10:21:51 -0300

    Assemblys

--- tests/Boleto.Tests/obj/Debug/net8.0/Boleto.Tests.AssemblyInfo.cs
+++ tests/Boleto.Tests/obj/Debug/net8.0/Boleto.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boleto.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887fab934d4875e7b62aff0d08470abad07e7a06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa616cf873386f988a5aad8fad836fd45276ec9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boleto.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boleto.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
